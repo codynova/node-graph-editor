80c0ae3c70d0c8688981334c27103091fd3168af
Cody Persinger <codypersinger@gmail.com>
2020-01-29 23:20:22 -0600

    Update test

--- test/index.ts
+++ test/index.ts
@@ -4,9 +4,10 @@ const init = async () => {
     const parent = document.createElement('div');
     const container = document.createElement('div');
     parent.appendChild(container);
-    const editor = new NodeEditor('test@0.0.1', container as HTMLElement);
-    editor.events['warn'] = [];
-    editor.events['error'] = [];
+    const editor = new NodeEditor('test@0.0.1', container);
+    editor.events.set('warn', []);
+    editor.events.set('error', []);
+    document.body.appendChild(parent);
 };
 
 init();
